a203bb8ff8cb64e5fc8d78ae4f99d4cdad1fc1af
sk10sk15 <sksk2525oreimo@gmail.com>
2019-12-27 12:36:48 +0900

    add days

--- 01/calendar_dev.rb
+++ 01/calendar_dev.rb
@@ -4,17 +4,23 @@
 
 
 require 'date'
+
 # 年月
 m = Date.today.strftime("%B")
 y = Date.today.strftime("%Y")
 z = [m , y]
-puts z.join(' ').center(25)
+puts z.join(' ').center(20)
 # 曜日
-puts %w[Sun Mon Tue Wed Thu Fri Sat].join(' ')
+puts %w[Su Mo Tu We Th Fr Sa].join(' ')
 
 # 1日と末日
 firstday = Date.new(Date.today.year, Date.today.month, 1)
 lastday = Date.new(Date.today.year, Date.today.month, -1)
 days = (1..lastday.day).map{ |n| n.to_s.rjust(2)}
 days = Array.new(firstday.wday, '  ').push(days).flatten.each_slice(7).to_a
-puts days
+
+
+days.each do |week|
+  puts week.join(' ')
+end
+
