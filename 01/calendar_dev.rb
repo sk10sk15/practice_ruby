bfa6015cfc56022ce0742ba084ed281844349df5
sk10sk15 <sksk2525oreimo@gmail.com>
2019-12-27 10:33:05 +0900

    update

--- 01/calendar_dev.rb
+++ 01/calendar_dev.rb
@@ -6,15 +6,15 @@
 require 'date'
 # 年月
 m = Date.today.strftime("%B")
-y = Date.today
-z = [m , y.year]
-puts z.join(' ')
+y = Date.today.strftime("%Y")
+z = [m , y]
+puts z.join(' ').center(25)
 # 曜日
 puts %w[Sun Mon Tue Wed Thu Fri Sat].join(' ')
 
 # 1日と末日
 firstday = Date.new(Date.today.year, Date.today.month, 1)
 lastday = Date.new(Date.today.year, Date.today.month, -1)
-for days in 1..lastday.day
-  puts days
-end 
+days = (1..lastday.day).map{ |n| n.to_s.rjust(2)}
+days = Array.new(firstday.wday, '  ').push(days).flatten.each_slice(7).to_a
+puts days
