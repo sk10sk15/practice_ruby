da35726b9d30238e1c0965bddb50e4f281ac1e0d
sk10sk15 <sksk2525oreimo@gmail.com>
2020-01-08 17:44:49 +0900

    説明を追加する

--- 01/calendar_dev.rb
+++ 01/calendar_dev.rb
@@ -2,18 +2,29 @@
 # dev用
 ###########
 
-
+#Dateクラスは組み込みクラスとなっていないため、使うにはまず宣言しないといけない。
 require 'date'
 
 # 年月
+#starftimeにフォーマット文字列を使って、文字列を出力できる。
+#%B だと月の名称(January, February ... )　%b だとその省略名(Jan, Feb ... ))
+#%Y は、西暦を表す数 todayなら、2020
 m = Date.today.strftime("%B")
 y = Date.today.strftime("%Y")
+
+#配列を作って、zに代入する。
 z = [m , y]
+#zを出力する。その際にjoinメソッドで文字列を結合させる。引数は空白にして区切りを作る。
+#centerメソッドは、引数の長さをもつ新しい文字列を作成し、文字列をその中央に置いて左右を空白で埋める。カレンダー上の真ん中に来るような引数は20だった。
 puts z.join(' ').center(20)
+
 # 曜日
+#%記法と呼ばれている。%wで配列を作ることができる。配列の要素はスペース区切りで指定する。
+#このままだと縦に出力されてしまうので、joinで結合させる。
 puts %w[Su Mo Tu We Th Fr Sa].join(' ')
 
 # 1日と末日
+#Date.newの引数は、(年,月,日)の順番
 firstday = Date.new(Date.today.year, Date.today.month, 1)
 lastday = Date.new(Date.today.year, Date.today.month, -1)
 days = (1..lastday.day).map{ |n| n.to_s.rjust(2)}
