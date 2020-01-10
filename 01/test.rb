054cfcf677300c31331dc87d1ba561c98b54a002
sk10sk15 <sksk2525oreimo@gmail.com>
2020-01-10 16:13:43 +0900

    コマンドライン引数入れたけど、integerじゃなくてStringになっている

--- 01/test.rb
+++ 01/test.rb
@@ -1,3 +1,14 @@
-ARGV.each_with_index do |arg, i|
-    puts "#{arg}"
-end
+require 'date'
+# Rubyの話
+# コマンドライン引数について調べてみる。
+# ARGVを使ったりするのかな
+# year:2020, month:6 の引数を与えるとして、どうやって年を認識する？
+# 今Date.today.yearとなっているものを、
+# Date.new(ARGV[0], ARGV[1), 1)
+# Date.new(2020, 6, 1)的な？
+# 使い方がわかっていない。
+
+year = ARGV[0]
+month = ARGV[1]
+y = Date.new(year, month, 1)
+puts y
