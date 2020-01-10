3f629f542afcf8eea13f4f5f5aa6c1adf3845360
sk10sk15 <sksk2525oreimo@gmail.com>
2020-01-10 18:42:52 +0900

    ボタン押すとconsole.log起動する

---
+++ 02/style.js
@@ -0,0 +1,10 @@
+//ボタンをクリックしたら、カレンダー表示する
+window.onload = function(){
+  document.getElementById("bottun").onclick = function() {
+    const weeks = ['日', '月', '火', '水', '木', '金', '土'];
+    console.log(weeks);
+  };
+}
+
+const weeks = ['日', '月', '火', '水', '木', '金', '土'];
+console.log(weeks);
