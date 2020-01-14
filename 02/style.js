2a3ed2775cf8c197571c619ace4976a4104480e1
sk10sk15 <sksk2525oreimo@gmail.com>
2020-01-14 18:20:11 +0900

    入力までは出来た

--- 02/style.js
+++ 02/style.js
@@ -1,8 +1,6 @@
-//ボタンをクリックしたら、カレンダー表示する
-// window.onload = function(){
-//   document.getElementById("bottun").onclick = function() {
-//     const weeks = ['日', '月', '火', '水', '木', '金', '土'];
-//     console.log(weeks);
-//   };
-// }
-
+function clickBtn1() {
+  const ta1 = document.form1.textarea1.value; //取得
+  document.getElementById("span1").textContent = ta1;　//取得したものをspanの中へ
+  const ta2 = document.form1.textarea2.value; //取得
+  document.getElementById("span2").textContent = ta2;　//取得したものをspanの中へ
+}
