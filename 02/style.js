a6c574b69ca9a17407c846b5d13b142ee46e263c
sk10sk15 <sksk2525oreimo@gmail.com>
2020-01-14 18:30:14 +0900

    クリアボタン作成。エラー表記も作成。

--- 02/style.js
+++ 02/style.js
@@ -1,6 +1,21 @@
+//表示するボタン
 function clickBtn1() {
   const ta1 = document.form1.textarea1.value; //取得
-  document.getElementById("span1").textContent = ta1;　//取得したものをspanの中へ
   const ta2 = document.form1.textarea2.value; //取得
-  document.getElementById("span2").textContent = ta2;　//取得したものをspanの中へ
+  if (ta2 === 0 || ta2 > 12 || ta2 < 1 || ta1 <= 0) {
+    document.getElementById("span0").textContent = "エラーです"
+    $(`.date`).remove();
+  }
+  else {
+    document.getElementById("span1").textContent = ta1;　//取得したものをspanの中へ
+    document.getElementById("span2").textContent = ta2;　//取得したものをspanの中へ
+  }
+}
+//クリアボタン
+function clickBtn2(){
+  document.getElementById("span0").textContent = "";
+  document.getElementById("span1").textContent = "";
+	document.getElementById("span2").textContent = "";
+  document.form1.textarea1.value = "";
+  document.form1.textarea2.value = "";
 }
