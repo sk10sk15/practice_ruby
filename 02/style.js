963062536d75fdecfa53e7e919dc73c7522846e5
sk10sk15 <sksk2525oreimo@gmail.com>
2020-01-14 16:39:38 +0900

    作り直す

--- 02/style.js
+++ 02/style.js
@@ -1,17 +1,8 @@
 //ボタンをクリックしたら、カレンダー表示する
-window.onload = function(){
-  document.getElementById("bottun").onclick = function() {
-    const weeks = ['日', '月', '火', '水', '木', '金', '土'];
-    console.log(weeks);
-  };
-}
+// window.onload = function(){
+//   document.getElementById("bottun").onclick = function() {
+//     const weeks = ['日', '月', '火', '水', '木', '金', '土'];
+//     console.log(weeks);
+//   };
+// }
 
-const weeks = ['日', '月', '火', '水', '木', '金', '土'];
-const date = new Date()
-const year = date.getFullYear()
-const month = date.getMonth()
-const startDate = new Date(year, month, 1) // 月の最初の日を取得
-const endDate = new Date(year, month + 1,  0) // 月の最後の日を取得, 0は前月の末日になるようだ
-console.log(month);
-console.log(startDate);
-console.log(endDate);
